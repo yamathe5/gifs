8fdbe46baf3a513d10a4aecd95e9380b77b68ce7
Yamathe5-laptop <manuelsegmar@gmail.com>
2024-03-20 18:05:28 -0500

    ADD: app component constructor, onInit & methods

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,55 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { GiphyService } from './services/giphy.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'gifs';
-}
+  gifs: any[] = []
+  searchHistory: string[] = [];
+  isHovered: boolean = false;
+
+
+  constructor(private giphy_service : GiphyService){}
+
+  ngOnInit(){
+    this.giphy_service.getTrendingGifs().subscribe({
+      next: (gifs) => {
+        this.gifs = gifs.data;
+        console.log(this.gifs);
+      }
+    })
+  }
+  onHover() {
+    this.isHovered = true;
+  }
+
+  onHoverExit() {
+    this.isHovered = false;
+  }
+  searchGifs(query: string) {
+    if (query.trim() === '') {
+      this.giphy_service.getTrendingGifs().subscribe({
+        next: (gifs) => {
+          this.gifs = gifs.data;
+        }
+      });
+    } else {
+      this.giphy_service.searchGifs(query).subscribe({
+        next: (response) => {
+          this.gifs = response.data;
+          if (!this.searchHistory.includes(query)) {
+            this.searchHistory.push(query);
+          }
+        }
+      });
+    }
+  
+}}
